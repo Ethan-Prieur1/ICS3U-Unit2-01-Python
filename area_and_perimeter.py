f86d35ff91474ac7ddd8d30b86dd53e81e96bfac
Ethan Prieur <ethan.prieur@mths.ca>
2022-02-25 19:49:44 +0000

    Added Circle Math File

---
+++ area_and_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Ethan Prieur
+# Created on February 25th 2022
+# This is a Math File with Radius
+
+import math
+
+
+def main():
+    # Function to calculate the area and perimeter of a circle
+
+    print("If a Circle has a Radius of 15 mm: ")
+    print("")
+    print("Area will be {} mm².".format(math.pi * 15**2))
+    print("Perimeter will be {} mm.".format(2 * math.pi * 15))
+    print("\nDone.")
+
+if __name__ == "__main__":
+    main()
